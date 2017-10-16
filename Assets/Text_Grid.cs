733aa1195994e62b65fa11fe3bdac6f56998e75f
sitsky <aris.miliotis@gmail.com>
2017-10-16 18:38:59 -0400

    More things move
    X slots in a line missing it and all disappear.
    Create new X now.

--- Assets/Text_Grid.cs
+++ Assets/Text_Grid.cs
@@ -6,56 +6,109 @@ public class Text_Grid : MonoBehaviour {
 
     static int ROWS = 20;
     static int COLUMNS = 11;
-
     float time_last_move = 0;
-
-
     Text grid_text;
     public static string[,] grid = new string[ROWS,COLUMNS];
+    
     // Use this for initialization
     void Start () {
-
         grid_text = GetComponent<Text>();
         grid_text.text = "";
         for (int i =0; i< ROWS; i++)
         {
-            for (int j=0; j< COLUMNS; j++)
+            for (int j = 0; j < COLUMNS; j++)
             {
-                grid[i, j] = "O";
+                if (i < ROWS - 1)
+                {
+                    grid[i, j] = "O";
+                }
+                else
+                {
+                    grid[i, j] = "G";
+                }
             }
         }
         grid_print();
-
-        grid[0, 5] = "X";
+        new_drop();
+        
+        for( int j = 0; j< COLUMNS; j++)
+        {
+            grid[17, j] = "X";
+        }
+        grid[17, 5] = "O"; 
     }
     // Update is called once per frame
     void Update() {
-        if (Time.time - time_last_move > 1)
+        if (Time.time - time_last_move > 0.5f)
         {
-            for (int i = 18; i >= 0; i--)
+            move_one_down();
+            time_last_move = Time.time;
+            for (int i = 0; i < ROWS-1; i++)
             {
-                for (int j = 0; j < COLUMNS; j++)
+                check_full_row(i);
+            }
+        }
+        grid_print();
+    }
+
+    public bool check_full_row(int i)
+    {
+        Debug.Log("check_full_row: " + i.ToString());
+        for (int j = 0; j < COLUMNS; j++)
+        {
+            if (!(grid[i, j].Equals("X")))
+                return false;
+        }
+        line_destroy_and_drop(i);
+        return true;
+    }
+
+    public void line_destroy_and_drop(int i)
+    {
+        Debug.Log("line_destroy_and_drop");
+        for (int j = 0; j < COLUMNS; j++)
+        {
+            grid[i, j] = "O";
+        }
+        move_one_down(i);
+    }
+
+    public void new_drop()
+    {
+        grid[0, 5] = "X";
+    }
+     
+    public void move_one_down(int k = 19)
+    {
+        for (int i = k - 2; i >= 0; i--)
+        {
+            for (int j = 0; j < COLUMNS; j++)
+            {
+                if (grid[i + 1, j].Equals("O"))
                 {
                     grid[i + 1, j] = grid[i, j];
                     grid[i, j] = "O";
                 }
+                else
+                {
+                    //new_drop();
+                }
             }
-            time_last_move = Time.time;
         }
-        grid_print();
-    } 
-    
+    }
+
+
     public void grid_print()
     {
         string to_print = "";
         for (int i = 0; i < ROWS; i++)
         {
-            for (int j= 0; j < COLUMNS; j++)
+            for (int j = 0; j < COLUMNS; j++)
             {
                 to_print = to_print + grid[i, j];
             }
             to_print = to_print + "\n";
         }
         grid_text.text = to_print;
-    } 
+    }
 }
