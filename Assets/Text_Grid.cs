e2352ff8860326866173738ab61c872032e717c1
sitsky <aris.miliotis@gmail.com>
2017-10-16 15:28:32 -0400

    X dropping now. Next step: Make it into a function.

--- Assets/Text_Grid.cs
+++ Assets/Text_Grid.cs
@@ -26,27 +26,36 @@ public class Text_Grid : MonoBehaviour {
         }
         grid_print();
 
-        grid[1, 5] = "X";
+        grid[0, 5] = "X";
     }
     // Update is called once per frame
     void Update() {
-        
         if (Time.time - time_last_move > 1)
         {
-            for (int i = 0; i <ROWS; i++)
+            for (int i = 18; i >= 0; i--)
             {
-                for (int j = 0; j<COLUMNS; j++)
+                for (int j = 0; j < COLUMNS; j++)
                 {
                     grid[i + 1, j] = grid[i, j];
+                    grid[i, j] = "O";
                 }
             }
+            time_last_move = Time.time;
         }
         grid_print();
-        time_last_move = Time.time;
     } 
     
     public void grid_print()
     {
-        grid_text.text = grid.ToString();
+        string to_print = "";
+        for (int i = 0; i < ROWS; i++)
+        {
+            for (int j= 0; j < COLUMNS; j++)
+            {
+                to_print = to_print + grid[i, j];
+            }
+            to_print = to_print + "\n";
+        }
+        grid_text.text = to_print;
     } 
 }
