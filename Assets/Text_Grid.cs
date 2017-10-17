153dc2326bc63fa0477ec929ef65be21b307a30e
sitsky <aris.miliotis@gmail.com>
2017-10-17 19:25:22 -0400

    Changed the controls system. Fixed more names.
    Next: Add the next piece. Rotations. Drop the Gs when line fills, now this will not happen.

--- Assets/Text_Grid.cs
+++ Assets/Text_Grid.cs
@@ -12,13 +12,14 @@ public class Text_Grid : MonoBehaviour {
     const int RIGHT_WALL = 12;
 
     static float DROP_EVERY_SECONDS = 0.5f;
-    int LEFT_OR_RIGHT = 0;
+   
     
     public enum STRIKE:int { WALL_HIT, GROUND_HIT, CLOCK_ROTATION, ANTICLOCK_ROTATION};
 
     public enum Pieces:int {L= 1, J, I, O, S, Z, T};
     int number_of_pieces = 7;
     Pieces piece_choice;
+    bool need_new_piece;
 
     float time_last_move = 0;
     Text grid_text;
@@ -48,143 +49,237 @@ public class Text_Grid : MonoBehaviour {
             }
         }
         grid_print();
-        new_drop();
+        need_new_piece = true;
+        new_piece();
     }
     // Update is called once per frame
     void Update() {
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            LEFT_OR_RIGHT = -1;
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            LEFT_OR_RIGHT = 1;
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        { }
-
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        { }
-
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                move_left();
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                move_right();
+            }
+        
         if (Time.time - time_last_move > DROP_EVERY_SECONDS)
         {
-            move_one_down(LEFT_OR_RIGHT);
-            LEFT_OR_RIGHT = 0;
-            time_last_move = Time.time;
+            move_all_one_down(GROUND);
             for (int i = 0; i < GROUND; i++)
             {
                 check_full_row(i);
             }
+            time_last_move = Time.time;
+            new_piece();
         }
         grid_print();
-
     }
 
-    public void move_one_down(int Dj, int k = GROUND)
+
+    public void move_left()
     {
-      //  bool need_new_piece = false;
         for (int i = GROUND - 1; i >= 0; i--)
+        {
+            if (grid[i, LEFT_WALL + 1].Equals("X"))
+            {
+                Debug.Log("X On edge");
+                return;
+            }
+        }
+
+        int[,] Xcoords = new int[4, 2];
+        int k = 0;
+        for (int j = LEFT_WALL + 1; j < RIGHT_WALL; j++)
+        {
+            for (int i = GROUND - 1; i >= 0; i--)
+            {
+                if (grid[i, j].Equals("X"))
+                {
+                    Xcoords[k, 0] = i;
+                    Xcoords[k, 1] = j;
+                    k++;
+                }
+            }
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            if ((grid[Xcoords[i, 0], Xcoords[i, 1] - 1].Equals("G")))
+            {
+                Debug.Log(grid[Xcoords[i, 0], Xcoords[i, 1] - 1] + "  " + Xcoords.ToString());
+                Debug.Log("No room to left");
+                return;
+            }
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            grid[Xcoords[i, 0], Xcoords[i, 1]] = " ";
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            Debug.Log(grid[Xcoords[i, 0], Xcoords[i, 1] - 1]);
+            grid[Xcoords[i, 0], Xcoords[i, 1] - 1] = "X";
+            Debug.Log(grid[Xcoords[i, 0], Xcoords[i, 1] - 1]);
+        }
+    }
+
+    public void move_right()
+    {
+        for (int i = GROUND - 1; i >= 0; i--)
+        {
+            if (grid[i, RIGHT_WALL - 1].Equals("X"))
+            {
+                Debug.Log("X On edge");
+                return;
+            }
+        }
+        int[,] Xcoords = new int[4, 2];
+        int k = 0;
+        for (int j = LEFT_WALL + 1; j < RIGHT_WALL - 1; j++)
+        {
+            for (int i = GROUND - 1; i >= 0; i--)
+            {
+                if (grid[i, j].Equals("X"))
+                {
+                    Xcoords[k, 0] = i;
+                    Xcoords[k, 1] = j;
+                    Debug.Log("X COORDS: " + Xcoords[k, 0].ToString() + "  " + Xcoords[k, 1].ToString());
+                    k++;
+                }
+            }
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            if ((grid[Xcoords[i, 0], Xcoords[i, 1] + 1].Equals("G")))
+            {
+                Debug.Log(grid[Xcoords[i, 0], Xcoords[i, 1] + 1] + "  " + Xcoords.ToString());
+                Debug.Log("No room to left");
+                return;
+            }
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            grid[Xcoords[i, 0], Xcoords[i, 1]] = " ";
+        }
+        for (int i = 0; i < 4; i++)
+        { 
+            grid[Xcoords[i, 0], Xcoords[i, 1] + 1] = "X";
+        }
+    }
+
+
+    public void move_all_one_down(int drop_above_rows)
+    {
+        int[,] Xcoords = new int[4, 2];
+        int k = 0;
+
+        for (int i = drop_above_rows - 1; i >= 0; i--)
         {
             for (int j = LEFT_WALL + 1; j < RIGHT_WALL; j++)
             {
                 if (grid[i, j].Equals("X"))
                 {
-                    if (grid[i + 1, j + Dj].Equals(" "))
-                    {
-                        grid[i + 1, j + Dj] = grid[i, j];
-                        grid[i, j] = " ";
-                    }
-                    else if (grid[i + 1, j].Equals("G"))
-                    {
-                        piece_behavior(STRIKE.GROUND_HIT);
-                        return;
-                    }
-                    else if (grid[i + 1, j + Dj].Equals("|"))
-                    {
-                        for (int m = 0; m < 5; m++)
-                        {
-                            move_one_down(0, i - 1);
-                        }
-                        return;
-                    }
+                    Xcoords[k, 0] = i;
+                    Xcoords[k, 1] = j;
+                    Debug.Log("X COORDS: " + Xcoords[k, 0].ToString() + "  " + Xcoords[k, 1].ToString());
+                    k++;
                 }
             }
         }
-        
-    }
 
-    public void piece_behavior(STRIKE what_hit, int i = GROUND - 1, int j = LEFT_WALL + 1)
-    {
-        if (what_hit == STRIKE.GROUND_HIT)
+        bool groundhit = false;
+        for (int i = 0; i < 4; i++)
         {
-            for (int k = i; k >= 0; k--)
+            if ((grid[Xcoords[i, 0] + 1, Xcoords[i, 1]].Equals("G")))
             {
-                for (int m = j; m < RIGHT_WALL; m++)
-                {
-                    if (grid[k, m].Equals("X"))
-                    {
-                        grid[k, m] = "G";
-                    }
-                }
+                groundhit = true;
+                need_new_piece = true;
             }
-            new_drop();
         }
-    }
-
-
-    public void new_drop()
-    {
-        piece_choice = (Pieces) Random.Range(1, number_of_pieces);
-        switch (piece_choice)
+        if (groundhit)
         {
-            case Pieces.L:
-                grid[0, 5] = "X"; grid[1, 5] = "X"; grid[2, 5] = "X"; grid[2, 6] = "X";
-                break; //L
-            case Pieces.J:
-                grid[0, 5] = "X"; grid[1, 5] = "X"; grid[2, 5] = "X"; grid[2, 4] = "X";
-                break; //J
-            case Pieces.I:
-                grid[0, 5] = "X"; grid[1, 5] = "X"; grid[2, 5] = "X"; grid[3, 5] = "X";
-                break; //I
-            case Pieces.O:
-                grid[0, 5] = "X"; grid[0, 6] = "X"; grid[1, 5] = "X"; grid[1, 6] = "X";
-                break; //O
-            case Pieces.S:
-                grid[0, 4] = "X"; grid[0, 5] = "X"; grid[1, 5] = "X"; grid[1, 6] = "X";
-                break; //S
-            case Pieces.Z:
-                grid[0, 5] = "X"; grid[0, 6] = "X"; grid[1, 5] = "X"; grid[1, 4] = "X";
-                break; //Z
-            case Pieces.T:
-                grid[0, 4] = "X"; grid[0, 5] = "X"; grid[0, 6] = "X"; grid[1, 5] = "X";
-                break; //T
-            default:
-                Debug.Log("New Piece Choice fail!");
-                break;
+            for (int i = 0; i < 4; i++)
+            {
+                grid[Xcoords[i, 0], Xcoords[i, 1]] = "G";
+            }
+        }
+        else
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                grid[Xcoords[i, 0], Xcoords[i, 1]] = " ";
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                grid[Xcoords[i, 0] + 1, Xcoords[i, 1]] = "X";
+            }
+        }
+    }
+            
+        
+    
+
+    public void new_piece()
+    {
+        if (need_new_piece )
+        {
+            need_new_piece = false;
+            piece_choice = (Pieces)Random.Range(1, number_of_pieces);
+            switch (piece_choice)
+            {
+                case Pieces.L:
+                    grid[0, 5] = "X"; grid[1, 5] = "X"; grid[2, 5] = "X"; grid[2, 6] = "X";
+                    break; //L
+                case Pieces.J:
+                    grid[0, 5] = "X"; grid[1, 5] = "X"; grid[2, 5] = "X"; grid[2, 4] = "X";
+                    break; //J
+                case Pieces.I:
+                    grid[0, 5] = "X"; grid[1, 5] = "X"; grid[2, 5] = "X"; grid[3, 5] = "X";
+                    break; //I
+                case Pieces.O:
+                    grid[0, 5] = "X"; grid[0, 6] = "X"; grid[1, 5] = "X"; grid[1, 6] = "X";
+                    break; //O
+                case Pieces.S:
+                    grid[0, 4] = "X"; grid[0, 5] = "X"; grid[1, 5] = "X"; grid[1, 6] = "X";
+                    break; //S
+                case Pieces.Z:
+                    grid[0, 5] = "X"; grid[0, 6] = "X"; grid[1, 5] = "X"; grid[1, 4] = "X";
+                    break; //Z
+                case Pieces.T:
+                    grid[0, 4] = "X"; grid[0, 5] = "X"; grid[0, 6] = "X"; grid[1, 5] = "X";
+                    break; //T
+                default:
+                    Debug.Log("New Piece Choice fail!");
+                    break;
+            }
         }
     }
 
-    public bool check_full_row(int i)
+    public bool check_full_row(int row)
     {
         //Debug.Log("check_full_row: " + i.ToString());
         for (int j = LEFT_WALL + 1; j < RIGHT_WALL; j++)
         {
-            if (!(grid[i, j].Equals("G")))
+            if (!(grid[row, j].Equals("G")))
                 return false;
         }
-        line_destroy_and_drop(i);
+        line_destroy_and_drop(row);
         return true;
     }
 
-    public void line_destroy_and_drop(int i)
+    public void line_destroy_and_drop(int row)
     {
         //Debug.Log("line_destroy_and_drop");
         for (int j = LEFT_WALL + 1; j < RIGHT_WALL; j++)
         {
-            grid[i, j] = " ";
+            grid[row, j] = " ";
         }
-        move_one_down(0, i);
+        move_all_one_down(row);
     }
 
     public void grid_print()
